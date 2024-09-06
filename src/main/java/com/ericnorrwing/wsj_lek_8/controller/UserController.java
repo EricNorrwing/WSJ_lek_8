264e290e08a9d04b5808b8230efe62d371fc32f8
EricNorrwing <eric.norrwing@gmail.com>
2024-09-06 10:43:31 +0200

    removed appl prop

--- src/main/java/com/ericnorrwing/wsj_lek_8/controller/UserController.java
+++ src/main/java/com/ericnorrwing/wsj_lek_8/controller/UserController.java
@@ -3,14 +3,12 @@ package com.ericnorrwing.wsj_lek_8.controller;
 import com.ericnorrwing.wsj_lek_8.model.User;
 import com.ericnorrwing.wsj_lek_8.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -25,6 +23,8 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> postUser(@PathVariable )
+    public ResponseEntity<User> postUser(@RequestBody User user) {
 
+        return ResponseEntity.ok(user);
+    }
 }
